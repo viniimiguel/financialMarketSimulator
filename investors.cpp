5ca71dac1b63099ad94f634e371f666b2e64118d
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2025-01-23 22:30:18 -0300

    investors

---
+++ investors.cpp
@@ -0,0 +1,1 @@
+#include "investors.h"
--- investors.h
+++ investors.h
@@ -1,11 +1,18 @@
 #pragma once
 #include "currency.h"
+#include <vector>
+#include "market.h"
 
-class investors 
-{
+class Investors {
 private:
-	double balance;
+	Currency* currencie;
+	struct Wallet {
+		std::string currencyName;
+		int amount;
+	};
+	std::vector<Wallet> currencies;
+
 public:
-
-
+	void sellCurrency(const std::string& currencyName, const std::string& investorsName, int amount);
+	void buyCurrency(const std::string& currencyName, const std::string& investorsName, int amount);
 };
